38644976b694f9e05bae1c9dd501a1d1c5574555
Indrajeet shinde <168427734+ims10@users.noreply.github.com>
2025-03-17 15:44:50 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,105 @@
+ <footer class="footer">
+
+    <section class="grid">
+
+       <div class="box">
+
+          <h3>quick links</h3>
+
+          <a href="home.php"> Home </a>
+          <a href="menu.php"> Menu </a>
+          <a href="orders.php"> Order </a>
+          <a href="about.php"> About </a>
+       </div>
+
+       <div class="box">
+          <h3> Cafe Shop <i class="fas fa-shopping-basket"></i> </h3>
+          <div class="share">
+             <a href="#" class="fab fa-facebook-f"></a>
+             <a href="#" class="fab fa-twitter"></a>
+             <a href="#" class="fab fa-instagram"></a>
+             <a href="#" class="fab fa-linkedin"></a>
+          </div>
+       </div>
+
+       <div class="box">
+
+          <h3>Opening Hours</h3>
+          <div class="dateinfo">
+             <p>MONDAY </p> <samp>CLOSED</samp>
+          </div>
+          <div class="dateinfo">
+             <p>TUESDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>WEDNESDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>THURSDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>FRIDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>SATURDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>SUNDAY </p><samp>*10.00 - 20.00</samp>
+          </div>
+
+
+       </div>
+
+       <div class="box">
+          <h3>contact info</h3>
+          <div class="phone">
+             <p> <i class="fas fa-phone"></i> +88015 0000 0000 </p>
+             <p> <i class="fas fa-phone"></i> +88017 0000 0000 </a>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+
+          <div class="phone1">
+             <h3>Branch Location</h3>
+
+             <p> <i class="fas fa-map-marker-alt"></i> Jahurul Islam City Gate, A/2 Jahurul Islam Ave, Dhaka 1212 </p>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+       </div>
+
+
+
+
+    </section>
+
+    <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span></span> | <a>Summer-2022 CSE347(3) (Group:2)</a> | all rights reserved!</div>
+
+ </footer>
+
+
+
+
+ <!-- footer section starts  
+
+<footer class="footer">
+
+   <div class="grid">
+
+      <div class="box1">
+         <h3> groco <i class="fas fa-shopping-basket"></i> </h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, saepe.</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+      </div>
+
+     
+     
+
+      
+
+</footer>
+
+footer section ends -->
